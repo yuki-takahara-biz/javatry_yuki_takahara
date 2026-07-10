04aca5b0c352cfca60322d5348b7696370729e17
yuki-takahara-biz <yuki.takahara@bizreach.co.jp>
2026-07-10 15:01:59 +0900

    7/10 作業分のコミット

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -47,7 +47,8 @@ public class Step01VariableTest extends PlainTestCase {
         String piari = null;
         String dstore = "mai";
         sea = sea + land + piari + ":" + dstore;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => mystic8:mai
+        // 髙原：null文字が空文字になることを理解しました。
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -56,7 +57,7 @@ public class Step01VariableTest extends PlainTestCase {
         String land = "oneman";
         sea = land;
         land = land + "'s dreams";
-        log(sea); // your answer? => 
+        log(sea); // your answer? => oneman
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -65,7 +66,7 @@ public class Step01VariableTest extends PlainTestCase {
         int land = 415;
         sea = land;
         land++;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 415
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -75,7 +76,10 @@ public class Step01VariableTest extends PlainTestCase {
         sea = land;
         sea = land.add(new BigDecimal(1));
         sea.add(new BigDecimal(1));
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 416
+        // 髙原：new が必要な時とそうでない時の違いについて知りたい。int は new が要らない。
+        // 髙原：Java の型について知りたいと思いました。合わせて、Java の型は Kotlin でも同様に使用可能なのか知りたいです。
+        // 髙原：BigDecimal が持っているメソッドについて知りたいと思いました。
     }
 
     // ===================================================================================
@@ -89,19 +93,24 @@ public class Step01VariableTest extends PlainTestCase {
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_String() {
         String sea = instanceBroadway;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 解りませんでした
+        // 髙原：instance variable とは何かを理解したい。
+        // 髙原：定義していないときは null という認識。
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_int() {
         int sea = instanceDockside;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => null => 違いました、0でした。
+        // 髙原：定義していない変数については、String の時 null になって、int の時は 0 になるという理解。
+        // 髙原：他の型だとどうなるのか気になる。
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_Integer() {
         Integer sea = instanceHangar;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 0 => 違いました、nullでした。
+        // 髙原：int と Integer の違いは何か
     }
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
@@ -110,7 +119,8 @@ public class Step01VariableTest extends PlainTestCase {
         instanceMagiclamp = "magician";
         helpInstanceVariableViaMethod(instanceMagiclamp);
         String sea = instanceBroadway + "|" + instanceDockside + "|" + instanceHangar + "|" + instanceMagiclamp;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => bigband|1||magician => bigband|1|null|magician でした。
+        // 髙原：null は「null」として表示されることを理解しました。
     }
 
     private void helpInstanceVariableViaMethod(String instanceMagiclamp) {
@@ -130,7 +140,8 @@ public class Step01VariableTest extends PlainTestCase {
         String sea = "harbor";
         int land = 415;
         helpMethodArgumentImmutableMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor
+        // 髙原：sea はグローバル変数ではないので関数を通しても変化しない
     }
 
     private void helpMethodArgumentImmutableMethodcall(String sea, int land) {
@@ -147,7 +158,8 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor => harbor416 でした。
+        // 髙原：なぜこうなるのか理解ができていないです
     }
 
     private void helpMethodArgumentMethodcall(StringBuilder sea, int land) {
@@ -163,7 +175,8 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentVariable(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor416 => harbor でした。
+        // 髙原：なぜこうなるのか理解できていないです。
     }
 
     private void helpMethodArgumentVariable(StringBuilder sea, int land) {
@@ -191,8 +204,13 @@ public class Step01VariableTest extends PlainTestCase {
      * o すべての変数をlog()でカンマ区切りの文字列で表示
      * </pre>
      */
+    private int piari;
     public void test_variable_writing() {
         // define variables here
+        String sea = "mystic";
+        Integer land = null;
+        String answer = sea + "," + land + "," + piari;
+        log(answer);
     }
 
     // ===================================================================================
@@ -204,11 +222,17 @@ public class Step01VariableTest extends PlainTestCase {
      * <pre>
      * _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
      * your question here (ここにあなたの質問を):
-     * 
+     * o ローカル変数、名前はa、初期値は"hoge"
+     * o インスタンス変数、名前はa（同じ）、初期値は"huga"
+     * o log()でで表示
      * _/_/_/_/_/_/_/_/_/_/
      * </pre>
      */
+    private String a = "hoge";
     public void test_variable_yourExercise() {
         // write your code here
+        String a = "huga";
+        log(a); // 答え：huga
+        //髙原：関数の中の変数の方が優先される。インスタンス変数の方にアクセスしたい場合はどうすればいい？
     }
 }
